650910ea7bd35ce461bdb13cd7562c9aa9c47d59
Andrei_Gridin <andreigridin918@gmail.com>
2024-12-11 18:02:40 +0500

    Добавьте файлы проекта.

---
+++ What_Where_When/What_Where_When.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    // Изменение кодировки кансоли
+    system("chcp 1251>nul");
+    // Изменяем цвет консоли и текста
+    system("color 80");
+    // Отображение соообщения в консоли
+    cout << "Програмируем на С++!" << endl;
+    // Задержка консоли окна
+    system("pause>nul");
+    return 0;
+}
