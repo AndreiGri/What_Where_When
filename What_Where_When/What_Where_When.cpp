f2e575758c4ae4384195f23b4fa45fe3758609d7
Andrei_Gridin <andreigridin918@gmail.com>
2025-01-05 00:50:29 +0500

    Максимально оптимизировал код и добавил проверку потоков

--- What_Where_When/What_Where_When.cpp
+++ What_Where_When/What_Where_When.cpp
@@ -4,61 +4,57 @@
 #include <vector>
 using namespace std;
 
-void searchQuestion(int& s, vector<bool>& vec)
+void searchQuestion(int& l, int s, vector<bool>& vec)                                   // Метод для поиска вопроса который ещё не был задан
 {
-	s = (s % vec.size());
-	while (vec[s] == false)
+	s = ((s + l) % vec.size());                                                         // Устраняем варианты не входящие в диапазон вектора vec
+	while (vec[s] == false)                                                             // Пока выбранный элемент вектора vec имеет значение false
 	{
-		s++;
-		s = (s % vec.size());
-	}
-	vec[s] = false;
-}
+		s++;                                                                            // переходим к следующему элементу вектора                                                     
+		s = (s % vec.size());                                                           // если значение s превышает диапазон элементов вектора
+	}                                                                                   // округляем это значение на величину вектора
+	vec[s] = false;                                                                     // После нахождения очередного элемента со значением true
+	l = s;                                                                              // присваиваем ему значение false и значение переменной s
+}                                                                                       // сохраняем в переменной l для использования вдальнейшем
 
-void scrolling(int& a, vector<bool>vec)
+void searchKakuelsh(string text, vector<int>& vec, int d = 1)                           // Метод для нахождения контрольных точек в файле
 {
-	a = (a % vec.size());
-}
-
-void searchKakuelsh(string text, vector<int>& vec, int d = 1)
-{
-	char ch;
-	int c = 0;
-	ifstream search;
-	search.open(text, ios::binary);
-	while (!search.eof())
+	char ch;                                                                            // Переменная для записи значений из потока
+	int c = 0;                                                                          // Счётчик контрольных точек
+	ifstream search;                                                                    // Объявляем поток
+	search.open(text, ios::binary);                                                     // Открываем поток
+	while (!search.eof())                                                               // Цикл пока не конец потока
 	{
-		search.get(ch);
-		if ((c % d) == 0 && ch == 0xA)
-		{
-			vec.push_back(search.tellg());
-			++c;
+		search.get(ch);                                                                 // Считываем элементы потока 
+		if ((c % d) == 0 && ch == 0xA)                                                  // Если остаток деления счётчика c на переменную d равен 0
+		{                                                                               // и считанный элемент является переносом строки
+			vec.push_back(search.tellg());                                              // добавляем в вектор vec номер считанного элемента
+			++c;                                                                        // увеличивае счётчик на 1
 		}
-		else
+		else                                                                            // иначе
 		{
-			if (ch == 0xA)
+			if (ch == 0xA)                                                              // если считанный элемент является переносом строки
 			{
-				++c;
+				++c;                                                                    // увеличивае счётчик на 1
 			}
 		}
 	}
 	search.close();
 }
 
-string reedOut(string text, int c, vector<int> vec)
+string reedOut(string text, int c, vector<int> vec)                                      // Метод для считывания текста из файла
 {
-	char buf[200];
-	ifstream questionAnswer;
-	questionAnswer.open(text, ios::binary);
-	questionAnswer.seekg(vec[c]);
-	questionAnswer.read(buf, sizeof(buf));
-	buf[vec[c + 1] - vec[c] - 2] = 0;
-	questionAnswer.close();
-	string str(buf);
-	return str;
+	char buf[200];                                                                       // Массив для записи текста
+	ifstream questionAnswer;                                                             // Объявляем поток для чтения из файла
+	questionAnswer.open(text, ios::binary);                                              // Открываем поток файла text
+	questionAnswer.seekg(vec[c]);                                                        // В потоке находим точку начала чтения взятую из вектора
+	questionAnswer.read(buf, sizeof(buf));                                               // Начинаем записывать в буфер из потока
+	buf[vec[c + 1] - vec[c] - 2] = 0;                                                    // Определяем точку конца записи из потока
+	questionAnswer.close();                                                              // Закрываем поток
+	string str(buf);                                                                     // Преобразуем буфер в строку
+	return str;                                                                          // Возвращаем строку
 }
 
-string answIn()
+string answIn()                                                                          // Метод для ввода выбранного ответа
 {
 	string text;
 	cout << " Скопируйте и вставьте верный ответ: ";
@@ -68,49 +64,59 @@ string answIn()
 
 int main()
 {
-	system("chcp 1251>nul");                                                                // Изменение кодировки кансоли
-	system("color 80");                                                                     // Изменяем цвет консоли и текста
-	int r = 0;                                                                              // Переменная для записи кол-ва правильных ответов
-	int w = 0;                                                                              // Переменная для записи кол-ва неверных ответов
-	int count = 0;                                                                          // Переменная для записи текущего значения
-	int as = 0;
-	vector<bool>veccount = { true,true,true,true,true };
-	int c = veccount.size();
-	string text1 = "Answer.txt";
-	string text2 = "Question.txt";
+	system("chcp 1251>nul");                                                             // Изменение кодировки кансоли
+	system("color 80");                                                                  // Изменяем цвет консоли и текста
+	int r = 0;                                                                           // Переменная для записи кол-ва правильных ответов
+	int w = 0;                                                                           // Переменная для записи кол-ва неверных ответов
+	string text1 = "Answer.txt";                                                         // Записываем в переменную адрес файла с ответами
+	string text2 = "Question.txt";                                                       // Записываем в переменную адрес файла с вопросами
+	ifstream ans(text1);
+	if (!ans)                                                                            // Проверка возможности открытия файла "Answer.txt"
+	{
+		cout << " Не удаётся открыть файл " << text1 << endl;                            // Если файл не откроется выводится сообщение и
+		return 1;                                                                        // программа заканчивается возвращая ошибку 1
+	}
+	ans.close();
+	ifstream quest(text1);
+	if (!quest)                                                                          // Проверка возможности открытия файла "Question.txt"
+	{
+		cout << " Не удаётся открыть файл " << text2 << endl;                            // Если файл не откроется выводится сообщение и
+		return 1;                                                                        // программа заканчивается возвращая ошибку 1
+	}
+	quest.close();
 	int ask = 0;                                                                         // Переменная записи значения смещения
-	int e = 0;                                                                           // Переменная хранения последнего сектора 
+	int lastNumber = 0;                                                                  // Переменная хранения последнего сектора 
 	vector<int>answ;                                                                     // Вектор для записи контрольных точек в ответах
 	vector<int>ques;                                                                     // Вектор для записи контрольных точек в вопросах
 	searchKakuelsh(text1, answ);                                                         // Вызов метода для записи контрольных точек ответов
 	searchKakuelsh(text2, ques, 4);                                                      // Вызов метода для записи контрольных точек вопросов
-	
-	while (c)
+	vector<bool>veccount;                                                                // Вектор для записи отвеченных вопросов
+	for (int i = 0; i < ques.size() - 1; i++)                                            // Заполняем вектор значениями true относительно
+	{                                                                                    // кол-ва вопросов
+		veccount.push_back(true);
+	}
+	int count = veccount.size();                                                         // Переменная равная кол-ву вопросов
+	while (count)                                                                        // Цикл до count == 0
 	{
-		cout << " Введите офсет (смещение) относительно текущего сектора на барабане: ";
-		cin >> ask;
-		scrolling(ask, veccount);
-		ask += e;
-		searchQuestion(ask, veccount);
-		e = ask;
-		string a = reedOut(text1, ask, answ);
-		cout << reedOut(text2, ask, ques) << endl;
-		string b = answIn();
-		if (a.compare(b) == 0)
+		cout << " Введите офсет относительно текущего сектора на барабане: ";            // Запрос офсета
+		cin >> ask;                                                                      // запись значения офсета в переменную
+		searchQuestion(lastNumber, ask, veccount);                                       // Вызов метода searchQuestion()
+		cout << reedOut(text2, lastNumber, ques) << endl;                                       // Выводим вопрос
+		if (reedOut(text1, lastNumber, answ).compare(answIn()) == 0)                            // Сравниваем ответ из метода reedOut() и метода answIn()
 		{
-			cout << " Верно! " << endl << endl;
-			r++;
+			cout << " Верно! " << endl << endl;                                          // Выводим если ответы одинаковы
+			r++;                                                                         // Инкркментируем переменную right
 		}
-		else
+		else                                                                             // Иначе выводим второй ответ
 		{
-			cout << " Не верно! Правильный ответ: " << a << endl << endl;
-			w++;
+			cout << " Не верно! Правильный ответ: " << reedOut(text1, lastNumber, answ) << endl << endl;
+			w++;                                                                         // Инкркментируем переменную wrong
 		}
-		c--;
+		count--;                                                                         // Декрементируем счётчик итераций
 	}
 
-	cout << " Верных ответов: " << r << endl;
-	cout << " Неверных ответов: " << w << endl;
+	cout << " Верных ответов: " << r << endl;                                            // Выводим кол-во правильных ответов
+	cout << " Неверных ответов: " << w << endl;                                          // Выводим кол-во не верных ответов
 
 	
 	system("pause>nul");                                                                 // Задержка консоли окна
